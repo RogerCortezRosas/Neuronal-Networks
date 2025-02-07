ac479d8d01fa6137336fdc8df2b33066e600bb1a
Rogelio Cortez <rcortezrosas@gmail.com>
2025-02-07 17:43:17 -0600

    Update

--- poyecto_lenguajeseñas.py
+++ poyecto_lenguajeseñas.py
@@ -54,8 +54,8 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 """## obtenemos la direcion de nuestras carpetas extraidas"""
 
-train_dir = "/tmp/sign-language-img/Train"
-test_dir = "/tmp/sign-language-img/Test"
+train_dir = "/tmp/Train"
+test_dir = "/tmp/Test"
 
 """## Data Generators
 Configuremos generadores de datos que leerán imágenes en nuestras carpetas de origen, las convertirán en tensores `float32` y las alimentarán (con sus etiquetas) a nuestra red. Tendremos un generador para las imágenes de entrenamiento y otro para las imágenes de validación. Nuestros generadores producirán lotes de imágenes de tamaño 28x28 y sus etiquetas (clases lenguaje de señas).
@@ -64,3 +64,179 @@ Configuremos generadores de datos que leerán imágenes en nuestras carpetas de 
 # Normalizacion de los datos para que los pixeles esten de 0-1
 train_datagen = ImageDataGenerator(rescale = 1/255)
 test_datagen = ImageDataGenerator(rescale = 1/255, validation_split= 0.2) # validation_split reserva 20% de los datos para validacion
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size = (28, 28), # estandarizar (cambia)el tamano de las matrices para evitar errores de tamanos
+    batch_size = 128, # Cantidad de imagenes juntas que van entrenarse
+    class_mode = "categorical",
+    color_mode = "grayscale", # Indica el color de las imagenes
+    subset = "training" # define a que corresponde es subset
+)
+
+validation_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size = (28, 28),
+    batch_size = 128,
+    class_mode = "categorical",
+    color_mode = "grayscale",
+    subset = "validation"
+)
+
+test_generator = test_datagen.flow_from_directory(
+    test_dir,
+    target_size = (28, 28),
+    batch_size = 128,
+    class_mode = "categorical",
+    color_mode = "grayscale"
+)
+
+"""# Creacion del modelo"""
+
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense,Input,ReLU,BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from  tensorflow.keras import models, optimizers, regularizers
+
+def model1():
+
+  model = models.Sequential([
+      Input(shape=(28,28,1)),
+
+      Conv2D(32,(2,2),padding="same"),
+      BatchNormalization(),
+      ReLU(),
+      MaxPooling2D((2,2)),
+
+      Conv2D(64,(2,2),padding="same"),
+      BatchNormalization(),
+      ReLU(),
+      MaxPooling2D((2,2)),
+
+      Conv2D(128,(2,2),padding="same"),
+      BatchNormalization(),
+      ReLU(),
+      MaxPooling2D((2,2)),
+
+      Flatten(),
+
+      Dense(128,activation='relu'),
+      Dropout(0.5),
+      Dense(24,activation='softmax')
+  ])
+
+
+  return model
+
+"""## Callback"""
+
+checkpoint = ModelCheckpoint('best_wights.keras',monitor='val_accuracy', verbose= 1, save_best_only=True)
+
+"""## Compilacion"""
+
+model_1 = model1()
+
+model_1.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+"""## Entrenamiento"""
+
+hist = model_1.fit(train_generator,
+                  steps_per_epoch=len(train_generator),
+                  epochs=50,
+                  validation_data=validation_generator,
+                  validation_steps=len(validation_generator),
+                  callbacks=[checkpoint])
+
+def visualitation(history):
+
+  fig , ax = plt.subplots(1,2,figsize=(15,5))
+
+  ax[0].plot(history.history['accuracy'],"go-",label='Entrenamiento accuracy')
+  ax[0].plot(history.history['val_accuracy'],"ro-",label='Validacion accuracy')
+  ax[0].set_title('Entrenamiento & validacion accuracy')
+  ax[0].legend()
+  ax[0].set_xlabel('Epochs')
+  ax[0].set_ylabel('Accuracy')
+
+  ax[1].plot(history.history['loss'],"go-",label='Entrenamiento loss')
+  ax[1].plot(history.history['val_loss'],"ro-",label='Validacion loss')
+  ax[1].set_title('Entrenamiento & validacion loss')
+  ax[1].legend()
+  ax[1].set_xlabel('Epochs')
+  ax[1].set_ylabel('Loss')
+  plt.show()
+
+visualitation(hist)
+
+"""# Ahora copiamos el modelo y le pasamos los mejores pesos que se registraron para evaluar con la informacion de test"""
+
+from keras.models import clone_model
+
+model1_2 = clone_model(model_1)
+
+model1_2.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+
+model1_2.load_weights('best_wights.keras')
+
+model1_2.evaluate(test_generator)
+
+"""# Keras Tuner
+
+Nos permite automatizar el proceso de configuracion de las redes neuronales
+"""
+
+!pip install -q -U keras-tuner
+
+import keras_tuner as kt
+from tensorflow import keras
+
+classes = [char for char in string.ascii_uppercase if char != "J" if char != "Z"]
+
+def constructor_modelos(hp):
+  # El objeto hp (generalmente una instancia de HyperParameters) permite definir y seleccionar diferentes valores de hiperparámetros durante el proceso de optimización.hp es un objeto de Keras Tuner que maneja los hiperparámetros.
+  model = tf.keras.models.Sequential()
+  model.add(tf.keras.layers.Conv2D(75, (3,3), activation= "relu", input_shape = (28, 28, 1)))
+  model.add(tf.keras.layers.MaxPool2D((2,2)))
+  model.add(tf.keras.layers.Flatten())
+
+  hp_units = hp.Int("units", min_value = 32, max_value = 512, step = 32)
+  model.add(tf.keras.layers.Dense(units=hp_units,activation = "relu", kernel_regularizer= regularizers.l2(1e-5)))
+  model.add(tf.keras.layers.Dropout(0.2))
+  model.add(tf.keras.layers.Dense(128,activation = "relu", kernel_regularizer= regularizers.l2(1e-5)))
+  model.add(tf.keras.layers.Dropout(0.2))
+  model.add(tf.keras.layers.Dense(len(classes), activation = "softmax"))
+
+  hp_learning_rate = hp.Choice('learning_rate', values = [1e-2, 1e-3, 1e-4])
+
+  model.compile(optimizer = keras.optimizers.Adam(learning_rate=hp_learning_rate),loss = "categorical_crossentropy", metrics = ["accuracy"])
+
+  return model
+
+tuner = kt.Hyperband(
+    #kt.Hyperband es un algoritmo de optimización de hiperparámetros que encuentra la mejor configuración para un modelo de Machine Learning de manera eficiente.
+    #Se basa en el algoritmo Hyperband, que optimiza el proceso de búsqueda de hiperparámetros al asignar más recursos a las configuraciones prometedoras y eliminar rápidamente las peores
+    constructor_modelos,
+    objective = "val_accuracy",
+    max_epochs = 20,
+    factor = 3, # Indica cuantos modelos pasaran a al siguiente ronda y el numero de epocas que aumentaran para medir a los que se quedaron
+    directory = "models/",
+    project_name = "platzi-tunner"
+)
+
+tuner.search(train_generator, epochs =20, validation_data = validation_generator) # Busqueda del mejor modelo
+
+best_hps = tuner.get_best_hyperparameters(num_trials =1)[0]
+
+print("Numero de neiuroanas conmejro calificacion",best_hps.get('units'))
+
+"""### Creacion del hypermodelo"""
+
+hypermodel = tuner.hypermodel.build(best_hps)
+
+history_hypermodel = hypermodel.fit(
+    train_generator,
+    epochs = 20,
+    callbacks = [callback_early],
+    validation_data = validation_generator
+)
